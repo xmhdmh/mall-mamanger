23dc6c02f1a5b5660b91306eec396d954bf12ed8
zyq <125396467@qq.com>
2016-09-09 11:11:04 +0800

    mapper 测试成功

--- src/main/java/com/mall/controller/ads/TAdsCategoryController.java
+++ src/main/java/com/mall/controller/ads/TAdsCategoryController.java
@@ -1,11 +1,16 @@
 package com.mall.controller.ads;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.github.pagehelper.PageInfo;
+import com.mall.dao.user.UserInfoMapper;
 import com.mall.model.ads.TAdsCategory;
+import com.mall.model.user.UserInfo;
 import com.mall.service.ads.ITAdsCategoryService;
 
 @RestController
