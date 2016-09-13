671773c055e02ef36e47421dbc56f097b80f8fac
zyq <125396467@qq.com>
2016-09-13 16:13:41 +0800

    修改管理员列表和个人信息页面，修改大部分页面名称

---
+++ src/main/java/com/mall/controller/UserController.java
@@ -0,0 +1,35 @@
+/**   
+* @Title: UserController.java
+* @Package com.mall.controller
+* @Description: TODO(用一句话描述该文件做什么)
+* @author 庄友权   
+* @date 2016年9月13日 下午3:48:04
+* @version V1.0   
+*/
+
+
+package com.mall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @ClassName: UserController
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author 庄友权
+ * @date 2016年9月13日 下午3:48:04
+ * 
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @RequestMapping("/userList")
+    public String userList(){
+        return "/userList";
+    }
+    
+    @RequestMapping("/userDetail")
+    public String userDetail(){
+        return "/userDetail";
+    }
+}
