fd97217fed5f239251bd337767a66de53d09212f
zyq <125396467@qq.com>
2016-12-12 10:45:54 +0800

    no

--- Admin/src/main/java/com/mall/WebApp.java
+++ Admin/src/main/java/com/mall/WebApp.java
@@ -1,14 +1,33 @@
 package com.mall;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class WebApp{
+public class WebApp implements WebApplicationInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebApp.class, args);
+    }
+
+    @Override
+    public void onStartup(ServletContext container) throws ServletException {
+        // TODO Auto-generated method stub
+        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
+        ServletRegistration.Dynamic registration = container.addServlet("dispatcher",
+                new DispatcherServlet(appContext));
+        registration.setLoadOnStartup(1);
+        registration.addMapping("*.request");
+    }
 }
